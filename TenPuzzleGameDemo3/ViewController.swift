0664b1a5bba449deccf4462e2f09db0f458485c7
anthrgrnwrld <anthrgrnwrld@gmail.com>
2015-06-01 16:07:07 +0900

    Initial Commit

---
+++ TenPuzzleGameDemo3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TenPuzzleGameDemo3
+//
+//  Created by Masaki Horimoto on 2015/06/01.
+//  Copyright (c) 2015年 Masaki Horimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
